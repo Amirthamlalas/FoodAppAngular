713fe736f6c5eca30833d2e22cd2c97fc463255b
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-25 08:31:07 +0530

    added dessert

---
+++ src/app/food-dessert/food-dessert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-dessert',
+  templateUrl: './food-dessert.component.html',
+  styleUrls: ['./food-dessert.component.css']
+})
+export class FoodDessertComponent {
+
+}
